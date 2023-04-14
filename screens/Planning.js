07339d36a55d754d7e5c9075722fa814cc5803f5
Your Name <you@example.com>
2023-04-14 08:44:26 +0200

    Planning mostly done

--- screens/Planning.js
+++ screens/Planning.js
@@ -1,33 +1,48 @@
-import React, {useState} from "react";
-import { View, SafeAreaView, Text, Button, StyleSheet } from 'react-native';
-
-
+import React, { useState } from "react";
+import { View, SafeAreaView, Text, StyleSheet, ScrollView } from 'react-native';
+import data from './planning.json';
 const Planning = () => {
+    try {
+        const [plan, setPlan] = useState([]);
 
-    
-    return (
-        <SafeAreaView style={{ padding: 10, flexDirection: 'row' }}>
-            <View style={styles.tableitem} id="planningtable"><Text>Datum</Text></View>
-            <View style={styles.tableitem} id="planningtable"><Text>Organisatie</Text></View>
-            <View style={styles.tableitem} id="planningtable"><Text>Activiteit</Text></View>
-            <View style={styles.tableitem} id="planningtable"><Text>Kosten</Text></View>
-        </SafeAreaView>
-    )
+        const planningdata = async () => {
+            const jsonfile = data.data;
+            const planElements = jsonfile.map(el => (
+                <View style={{ padding: 10 }} key={el.id}>
+                    <View style={styles.tableitem} id="planningtable"><Text>{el.date}</Text></View>
+                    <View style={styles.tableitem} id="planningtable"><Text>{el.organisers}</Text></View>
+                    <View style={styles.tableitem} id="planningtable"><Text>{el.activity}</Text></View>
+                    <View style={styles.tableitem} id="planningtable"><Text>€{el.cost}</Text></View>
+                </View>
+            ));
+            setPlan(planElements);
+        };
+
+        React.useEffect(() => {
+            planningdata();
+        }, []);
+        return (
+            <SafeAreaView style={{margin: 0, marginBottom: 45, alignItems: 'center'}}>
+                <ScrollView>
+                {plan}
+                </ScrollView>
+            </SafeAreaView>
+        );
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#000',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: '#fff'
-    },
     tableitem: {
         backgroundColor: '#0ff',
         borderColor: '#000',
-        borderWidth: 3
+        borderWidth: 1,
+        padding: 2,
+        margin: 0,
+        maxWidth: 180,
+        minWidth: 50,
     }
-  });
+});
 
 export default Planning;
