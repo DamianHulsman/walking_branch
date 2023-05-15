d4063fdc3e4fd9659c14801ac8a08fae07cb96b8
Your Name <you@example.com>
2023-05-15 10:28:29 +0200

    blablabla

--- screens/Planning.js
+++ screens/Planning.js
@@ -7,7 +7,7 @@ const Planning = () => {
 
     const planningdata = async () => {
         try {
-          const plandata = await axios.get('http://192.168.2.63/WalkingBranchAPI/server.php?fn=getPlanning');
+          const plandata = await axios.get('http://10.151.87.224/WalkingBranchAPI/server.php?fn=getPlanning');
           const planElements = plandata.data.map(el => (
             <View style={{ padding: 10, height: 200, flex: 1 }} key={el.id}>
               <View style={styles.tableitem} id="planningtable"><Text>{el.date}</Text></View>
@@ -15,7 +15,30 @@ const Planning = () => {
               <View style={styles.tableitem} id="planningtable"><Text>{el.activity}</Text></View>
               <View style={styles.tableitem} id="planningtable"><Text>€{el.cost}</Text></View>
               <View style={[styles.tableitem, {alignItems: 'center', alignSelf: 'center', display: 'flex', flexDirection: 'row', justifyContent: 'center', flex: 1}]} id="planningtable">
-                <Button style={{flex: 1}} onPress={() => { alert(`Opkomst:\nDatum: ${el.date}\nOrganisatie: ${el.organisatie}\nActiviteit: ${el.activity}\nKosten: €${el.cost}\nNotities: ${el.notes}`); }} title="    View    " />
+                <Button 
+                style={{flex: 1}} 
+                onPress={() => { 
+                  // alert(`
+                  // Opkomst:\n
+                  // Datum: ${el.date}\n
+                  // Organisatie: ${el.organisatie}\n
+                  // Activiteit: ${el.activity}\n
+                  // Kosten: €${el.cost}\n
+                  // Notities: ${el.notes}
+                  // `);
+                  Alert.alert(
+                    'Opkomst',
+                    `Datum: ${el.date}\n
+                    Organisatie: ${el.organisatie}\n
+                    Activiteit: ${el.activity}\n
+                    Kosten: ${el.cost}\n
+                    Notities: ${el.notes}`,
+                    [
+                      { text: 'OK'},
+                    ],
+                    { cancelable: false }
+                  );
+                  }} title="    View    " />
                 <Button style={{flex: 1}} disabled={el.disabled} onPress={() => { showAlert(el.id); }} title="   Delete   " />
               </View>
             </View>
@@ -37,9 +60,9 @@ const Planning = () => {
           'Weet je zeker dat je deze opkomst wil verwijderen?',
           [
             { text: 'Cancel', style: 'cancel' },
-            { text: 'OK', onPress: async () => {await axios.get(`http://192.168.2.63/WalkingBranchAPI/server.php?fn=deleteItem&id=${deleteid}`); planningdata; }},
+            { text: 'OK', /*onPress: async () => {await axios.get(`http://192.168.2.63/WalkingBranchAPI/server.php?fn=deleteItem&id=${deleteid}`); planningdata; }*/},
           ],
-          { cancelable: false }
+          { cancelable: true }
         );
       };
 
